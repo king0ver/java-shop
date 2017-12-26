0ef84b9f03af0344e4b78d62538b33d2ffe0e162
jianjianming <13100207263@163.com>
2017-12-26 14:53:01 +0800

    服务设施

---
+++ nanshan-core/src/main/java/com/enation/app/nanshan/core/action/ServiceController.java
@@ -0,0 +1,101 @@
+package com.enation.app.nanshan.core.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import net.sf.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.enation.app.nanshan.core.service.IArticleManager;
+import com.enation.app.nanshan.core.service.ICatManager;
+import com.enation.app.nanshan.model.ArticleCat;
+import com.enation.app.nanshan.model.NanShanArticleVo;
+import com.enation.framework.action.GridController;
+import com.enation.framework.action.JsonResult;
+import com.enation.framework.util.JsonResultUtil;
+
+/**
+ * 服务管理
+ * @author LiJM
+ *
+ */
+@Controller 
+@Scope("prototype")
+@RequestMapping("/core/admin/service")
+public class ServiceController extends GridController{
+	
+	@Autowired
+	private IArticleManager  articleManager;
+
+	@Autowired
+	private ICatManager catManager;
+	
+	/**
+	 * 跳转到修改页面
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value="/edit")
+	public ModelAndView edit(Integer catId){
+		ModelAndView view = new ModelAndView();
+		List<ArticleCat> catList = catManager.queryCatChildrenInfoByCatIds("46");
+		List<NanShanArticleVo> list = new ArrayList<NanShanArticleVo>();
+		if(null == catId){
+			if(catList != null && catList.size()>0){
+				Long articleCatId = catList.get(0).getCat_id();
+				list = articleManager.queryArticleByCatId(articleCatId.intValue());
+			}
+		}else{
+			list = articleManager.queryArticleByCatId(catId);
+		}
+		if(list!=null && list.size()>0){
+			view.addObject("data",list.get(0));
+		}else{
+			NanShanArticleVo articleVo = new NanShanArticleVo();
+			if(catId != null){
+				articleVo.setCat_id(catId);
+			}
+			view.addObject("data", articleVo);
+		}
+		view.addObject("catList", catList);
+		view.setViewName("/nanshan/admin/service/page");
+		return view;
+	}
+	
+	/**
+	 * 修改
+	 * @param spec
+	 * @param specValName
+	 * @param spec
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="/save-edit")
+	public JsonResult saveEdit(NanShanArticleVo articleVo,HttpServletRequest request){
+		//保存修改
+		try{
+			String address = request.getParameter("address");
+			JSONObject json = new JSONObject();
+			json.put("address",address);
+			json.put("content", articleVo.getContent());
+			articleVo.setContent(json.toString());
+			if(articleVo.getId()>0){
+				articleManager.updateArticle(articleVo);
+			}else{
+				articleManager.addArticle(articleVo);
+			}
+			return JsonResultUtil.getSuccessJson("操作成功");
+		}catch(Exception e){
+			return JsonResultUtil.getErrorJson("操作失败:"+e.getMessage());
+		}		
+	}
+	
+}
