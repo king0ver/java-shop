7b4d5c53f075de3de1db9137d1aa8e063758efc5
xiu <xiu@F-09>
2018-01-04 19:51:37 +0800

    服务设施分类添加删除

--- nanshan-core/src/main/java/com/enation/app/nanshan/core/service/impl/CatManagerImpl.java
+++ nanshan-core/src/main/java/com/enation/app/nanshan/core/service/impl/CatManagerImpl.java
@@ -39,4 +39,23 @@ public class CatManagerImpl implements ICatManager {
 		return daoSupport.queryForList(sql, ArticleCat.class);
 	}
 
+	@Override
+	public void addCat(ArticleCat articleCat) {
+		int id=this.daoSupport.queryForInt("select max(cat_id) from  es_nanshan_article_category", null);
+		articleCat.setCat_id(id+1);
+		String url="/nanshan/facility.html?catId="+articleCat.getCat_id();
+		articleCat.setPc_url(url);
+		articleCat.setWap_url(url);
+		daoSupport.insert("es_nanshan_article_category", articleCat);
+		
+	}
+
+	@Override
+	public void delCat(int id) {
+		if(id>0){
+		daoSupport.execute("delete from es_nanshan_article_category where cat_id="+id);
+		}
+		
+	}
+
 }
